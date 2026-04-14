eb45df1efc235392fbb343fb96518f773b6672fc
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 17:37:14 +0530

    Create UserServiceImpl.java

---
+++ src/main/java/com/kb/fashionhouse/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.kb.fashionhouse.service;
+
+import com.kb.fashionhouse.model.User;
+import com.kb.fashionhouse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
