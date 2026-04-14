8993b7a1329befcfe71803e0cfc75046659b9ccc
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 21:54:38 +0530

    Update CustomUserDetailsService.java

--- src/main/java/com/kb/fashionhouse/service/CustomUserDetailsService.java
+++ src/main/java/com/kb/fashionhouse/service/CustomUserDetailsService.java
@@ -1,32 +1,23 @@
-package com.kb.fashionhouse.service;
+@Override
+public UserDetails loadUserByUsername(String input) {
 
-import com.kb.fashionhouse.model.User;
-import com.kb.fashionhouse.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.stereotype.Service;
+    User user = userRepository.findByUsername(input);
 
-import java.util.Collections;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = userRepository.findByUsername(username);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.emptyList()
-        );
+    if (user == null) {
+        user = userRepository.findByEmail(input);
     }
-}  
+
+    if (user == null) {
+        user = userRepository.findByMobile(input);
+    }
+
+    if (user == null) {
+        throw new UsernameNotFoundException("User not found");
+    }
+
+    return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            new ArrayList<>()
+    );
+}
