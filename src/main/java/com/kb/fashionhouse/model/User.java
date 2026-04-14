f469778b6e2df6d1cd1ef8e46b20ea8fbb728a0e
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 22:14:50 +0530

    Update User.java

--- src/main/java/com/kb/fashionhouse/model/User.java
+++ src/main/java/com/kb/fashionhouse/model/User.java
@@ -10,10 +10,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
-
+    @Column(unique = true)
     private String email;
 
+    @Column(unique = true)
     private String mobile;
 
     private String password;
@@ -22,17 +22,35 @@ public class User {
 
     // getters & setters
 
-    public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getMobile() { return mobile; }
-    public void setMobile(String mobile) { this.mobile = mobile; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
