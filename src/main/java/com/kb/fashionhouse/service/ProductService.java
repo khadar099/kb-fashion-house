5f95d2b5d2d3ef1e42472598ee37a089b05333cc
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 17:25:01 +0530

    Create ProductService.java

---
+++ src/main/java/com/kb/fashionhouse/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.kb.fashionhouse.service;
+
+import com.kb.fashionhouse.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+}
