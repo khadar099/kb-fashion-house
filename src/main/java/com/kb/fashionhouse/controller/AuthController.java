15d2cf8d2190fde491079945311678992d5ef12a
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 17:37:37 +0530

    Create AuthController.java

---
+++ src/main/java/com/kb/fashionhouse/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.kb.fashionhouse.controller;
+
+import com.kb.fashionhouse.model.User;
+import com.kb.fashionhouse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
+}
