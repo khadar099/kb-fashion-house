c7c186f7196a33da061961f9d067a01d7071ba60
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-14 17:36:07 +0530

    Create UserRepository.java

---
+++ src/main/java/com/kb/fashionhouse/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kb.fashionhouse.repository;
+
+import com.kb.fashionhouse.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
