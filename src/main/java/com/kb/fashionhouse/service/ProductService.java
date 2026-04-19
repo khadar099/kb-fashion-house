3fecb2495643fe1337b0c468caea6571c1f633d1
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2026-04-19 23:06:16 +0530

    Update ProductService.java

--- src/main/java/com/kb/fashionhouse/service/ProductService.java
+++ src/main/java/com/kb/fashionhouse/service/ProductService.java
@@ -4,5 +4,14 @@ import com.kb.fashionhouse.model.Product;
 import java.util.List;
 
 public interface ProductService {
+
+    // =========================
+    // GET ALL PRODUCTS
+    // =========================
     List<Product> getAllProducts();
+
+    // =========================
+    // GET PRODUCT BY ID
+    // =========================
+    Product getProductById(Long id);
 }
